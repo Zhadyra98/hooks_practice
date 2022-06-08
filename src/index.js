07e7d84bdb96f371124d9295c7bb56c44d21115b
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-06-08 19:24:38 +0300

    feat: useRef hook implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App2 from './App2';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <App/>,
+    <App2/>,
   document.getElementById('root')
 );
 
